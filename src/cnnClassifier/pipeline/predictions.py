816a3c6bdc3f67e07e1c6b902483caead428ee67
AlquamaSayyed <59507223+AlquamaSayyed@users.noreply.github.com>
2024-03-21 11:18:48 +0530

    final commit with bug fix in prediction

--- src/cnnClassifier/pipeline/predictions.py
+++ src/cnnClassifier/pipeline/predictions.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import os
-
+from PIL import Image
 
 class PredictionPipeline:
     def __init__(self, filename):
@@ -12,9 +12,13 @@ class PredictionPipeline:
         model = tf.keras.models.load_model(os.path.join("artifacts","training", "model.h5"))
 
         imagename = self.filename
-        test_image = tf.keras.preprocessing.image.load_img(imagename, target_size=(224, 224))
-        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis=0)
+        img = Image.open(imagename)
+        img = img.resize((244, 244), resample=Image.LANCZOS)
+        img = np.array(img) / 255.0  # Normalize pixel values to [0, 1]
+        test_image = np.expand_dims(img, axis=0)
+        # test_image = tf.keras.preprocessing.image.load_img(imagename)
+        # test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+        # test_image = np.expand_dims(test_image, axis=0)
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
